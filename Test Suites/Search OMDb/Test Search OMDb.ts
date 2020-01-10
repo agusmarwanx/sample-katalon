f40b1a5ba9eab7772d6c3de849b83ac753e497cb
ndoroagus <ndoroagus@192.168.2.242>
2020-01-10 15:32:47 +0700

    add test suite

---
+++ Test Suites/Search OMDb/Test Search OMDb.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Search OMDb</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>28505ea6-4b3b-4357-9f15-3cc9617c84f5</testSuiteGuid>
+   <testCaseLink>
+      <guid>1f337984-72d9-48b2-845f-e63136643654</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Search/1. Positive - Get Data Search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>12854e16-be08-43b2-8d24-92acf205d959</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Search/2. Negative - No Search</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5e2c8279-309a-4712-b6b7-de46d8b2abdb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Search/3. Negative - Invalid key</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
