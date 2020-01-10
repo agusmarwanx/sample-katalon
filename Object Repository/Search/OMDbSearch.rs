38c960768e6493a05a10354c244896cb6287ed74
ndoroagus <ndoroagus@192.168.2.242>
2020-01-10 13:58:04 +0700

    add omdb api

---
+++ Object Repository/Search/OMDbSearch.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>OMDbSearch</name>
+   <tag></tag>
+   <elementGuidId>78a66fb4-b175-48a2-83db-f29ff7d2a5a1</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>http://www.omdbapi.com/?apikey=19f95e90&amp;s=criminal</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
